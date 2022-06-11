7d3ebf65a3592e745fbad53f629d5c80be721fa8
John Rebbeck <admin@makerzone.com.au>
2022-06-11 10:25:08 +1000

    [fix] Changed MqttConnectionFailure page to use settings from central system settings file instead of web.config

--- src/GrowSense.SystemManager.WWW/MqttConnectionFailure.aspx.cs
+++ src/GrowSense.SystemManager.WWW/MqttConnectionFailure.aspx.cs
@@ -1,24 +1,33 @@
 using System.Configuration;
 using System.Web.Configuration;
+using GrowSense.SystemManager.Common;
+using System.IO;
 
 namespace GrowSense.SystemManager.WWW
 {
-  using System;
-  using System.Web;
-  using System.Web.UI;
+    using System;
+    using System.Web;
+    using System.Web.UI;
 
-  public partial class MqttConnectionFailure : System.Web.UI.Page
-  {
-    public string MqttHost;
-    public string MqttUsername;
-    public int MqttPort;
-
-    public void Page_Load (object sender, EventArgs e)
+    public partial class MqttConnectionFailure : System.Web.UI.Page
     {
-      MqttHost = WebConfigurationManager.AppSettings ["MqttHost"];
-      MqttUsername = WebConfigurationManager.AppSettings ["MqttUsername"];
-      MqttPort = Convert.ToInt32 (WebConfigurationManager.AppSettings ["MqttPort"]);
-    }
-  }
-}
+        public string MqttHost;
+        public string MqttUsername;
+        public int MqttPort;
 
+        public SettingsManager Manager;
+        public SystemSettings Settings;
+
+        public void Page_Load(object sender, EventArgs e)
+        {
+            var indexDirectory = Path.GetFullPath(WebConfigurationManager.AppSettings["IndexDirectory"]);
+
+            Manager = new SettingsManager(indexDirectory);
+            Settings = Manager.LoadSettings();
+
+            MqttHost = Settings.MqttHost;
+            MqttUsername = Settings.MqttUsername;
+            MqttPort = Settings.MqttPort;
+        }
+    }
+}
