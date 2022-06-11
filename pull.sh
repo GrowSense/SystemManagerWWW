94c3a3d9a2b2f7a08cced9765437d8d357a03b45
John Rebbeck <admin@makerzone.com.au>
2022-06-11 10:24:32 +1000

    Added pull local to pull.sh

--- pull.sh
+++ pull.sh
@@ -1,5 +1,7 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
+git pull local $BRANCH || echo "Failed to pull from lan repository"
+
 git pull lan $BRANCH || echo "Failed to pull from lan repository"
 
 git pull origin $BRANCH
